d53ce5ebff766e3ee5c820b87082b777d312c3c1
Daniel Olaszy <daniel.olaszy@gmail.com>
2021-07-15 14:30:38 +0100

    Added react router and nav links to switch inbetween

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 // import logo from './logo.svg';
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Axios from "axios";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function Profession({ profession }) {
+  return (
+    <>
+      <section>
+        <header className="border-bottom py-1 my-3">
+          <h1>{profession.profession.name}</h1>
+        </header>
+        <article></article>
+      </section>
+    </>
+  );
+}
+
+function Overview({ profession }) {
+  return (
+    <>
+      <section>
+        <header className="border-bottom py-1 my-3">
+          <h1>Overview</h1>
+        </header>
+        <article></article>
+      </section>
+    </>
+  );
+}
 
 function App() {
   const [profileRegion, setProfileRegion] = useState("us");
@@ -38,28 +65,56 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <div className="row m-3 justify-content-center">
-        <div className="col-xxl-3 col-xl-4 col-lg-5 col-md-6 col-sm-7 col-8 p-2 d-grid gap-2">
-          <div>
-            <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Character"></input>
+    <Router>
+      <div className="container">
+        <nav className="nav nav-pills nav-justified">
+          <Link className="nav-link" to="/overview">
+            Overview
+          </Link>
+          {profileProfessions.map((profession) => {
+            return (
+              <Link key={profession.profession.id} className="nav-link" to={"/" + profession.profession.name}>
+                {profession.profession.name}
+              </Link>
+            );
+          })}
+        </nav>
+
+        <div className="row m-3 justify-content-center">
+          <div className="col-xxl-3 col-xl-4 col-lg-5 col-md-6 col-sm-7 col-8 p-2 d-grid gap-2">
+            <div>
+              <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Character"></input>
+            </div>
+            <div>
+              <input className="form-control" list="datalistOptions" id="exampleDataList" placeholder="Realms"></input>
+              <datalist id="datalistOptions">
+                <option value="Kelthuzad"></option>
+                <option value="Ravencrest"></option>
+                <option value="Outland"></option>
+                <option value="Tichondrious"></option>
+                <option value="Chicago"></option>
+              </datalist>
+            </div>
+            <button className="btn btn-primary" type="button" onClick={getProfileProfessions}>
+              Button
+            </button>
           </div>
-          <div>
-            <input className="form-control" list="datalistOptions" id="exampleDataList" placeholder="Realms"></input>
-            <datalist id="datalistOptions">
-              <option value="Kelthuzad"></option>
-              <option value="Ravencrest"></option>
-              <option value="Outland"></option>
-              <option value="Tichondrious"></option>
-              <option value="Chicago"></option>
-            </datalist>
-          </div>
-          <button className="btn btn-primary" type="button" onClick={getProfileProfessions}>
-            Button
-          </button>
         </div>
       </div>
-    </div>
+
+      <Switch>
+        <Route exact path="/overview" component={Overview} />
+        {profileProfessions.map((profession) => {
+          return (
+            <Route
+              key={profession.profession.id}
+              path={"/" + profession.profession.name}
+              render={() => <Profession profession={profession} />}
+            />
+          );
+        })}
+      </Switch>
+    </Router>
   );
 }
 
